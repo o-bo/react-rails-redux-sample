b64a27ac2bf4e64fa2485ba548266d3704d88b03
o-bo <olivier.berho@gmail.com>
2016-02-19 15:12:02 +0100

    [*] Figaro for secret and token.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ReactRailsReduxSample
   class Application < Rails::Application
+
+    config.secret_key_base = Figaro.env.secret_key_base
+    config.secret_token = Figaro.env.secret_token
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
