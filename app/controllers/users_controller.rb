e1f9729e07840841eda01c80809db8dd6e8ab38f
o-bo <olivier.berho@gmail.com>
2016-02-24 18:15:49 +0100

    [*] Render users in json.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
-  def index ; end
+  before_action :authenticate_user!
+
+  def index
+    respond_to do |format|
+      format.html {}
+      format.json { render json: Users.all.to_json }
+    end
+  end
 end
