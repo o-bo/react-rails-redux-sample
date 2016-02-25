71343cfef011fb30ca883dd2dae13d67d458273d
o-bo <olivier.berho@gmail.com>
2016-02-25 15:32:02 +0100

    [+] Add method for creating a new user in users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,22 @@ class UsersController < ApplicationController
       format.json { render json: User.all }
     end
   end
+
+
+  def create
+    user = User.create permit_params
+
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def permit_params
+    params.require(:user).permit(:email, :password)
+  end
 end
