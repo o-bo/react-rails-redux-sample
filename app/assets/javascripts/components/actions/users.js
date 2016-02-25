a11dfc6ed08cc2a40d86d1fc54539028505bd5d4
o-bo <olivier.berho@gmail.com>
2016-02-25 15:51:00 +0100

    [*] Pass method and body to middleware when fetching and posting users.

--- app/assets/javascripts/components/actions/users.js
+++ app/assets/javascripts/components/actions/users.js
@@ -1,15 +1,30 @@
-import { CALL_API } from '../middleware/api'
+import { CALL_API, GET, POST } from '../middleware/api'
 
 export const USERS_FETCH_REQUEST = 'USERS_FETCH_REQUEST'
 export const USERS_FETCH_SUCCESS = 'USERS_FETCH_SUCCESS'
 export const USERS_FETCH_FAILURE = 'USERS_FETCH_FAILURE'
 
-// Uses the API middlware to get a quote
 export function fetchUsers() {
   return {
     [CALL_API]: {
       endpoint: 'users',
+      method: GET,
       types: [USERS_FETCH_REQUEST, USERS_FETCH_SUCCESS, USERS_FETCH_FAILURE]
+    }
+  }
+}
+
+export const USERS_POST_REQUEST = 'USERS_POST_REQUEST'
+export const USERS_POST_SUCCESS = 'USERS_POST_SUCCESS'
+export const USERS_POST_FAILURE = 'USERS_POST_FAILURE'
+
+export function postUser(user) {
+  return {
+    [CALL_API]: {
+      endpoint: 'users',
+      method: POST,
+      body: user,
+      types: [USERS_POST_REQUEST, USERS_POST_SUCCESS, USERS_POST_FAILURE]
     }
   }
 }
