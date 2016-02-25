7f09d727de407c65226e97b3b570849356634111
o-bo <olivier.berho@gmail.com>
2016-02-25 15:37:22 +0100

    [+] Export constants for api.

--- app/assets/javascripts/components/middleware/api.js
+++ app/assets/javascripts/components/middleware/api.js
@@ -25,6 +25,10 @@ function callApi(endpoint) {
 }
 
 export const CALL_API = Symbol('Call API')
+export const GET = 'get'
+export const POST = 'post'
+export const PUT = 'put'
+export const DELETE = 'delete'
 
 export default store => next => action => {
 
