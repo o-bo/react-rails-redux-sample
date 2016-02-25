3a77fc00203bc1e883035c0c9de20ff46afdfc25
o-bo <olivier.berho@gmail.com>
2016-02-25 14:10:29 +0100

    [+] Actions for fetching users.

--- app/assets/javascripts/components/actions/users.js
+++ app/assets/javascripts/components/actions/users.js
@@ -1,1 +1,15 @@
+import { CALL_API } from '../middleware/api'
 
+export const USERS_FETCH_REQUEST = 'USERS_FETCH_REQUEST'
+export const USERS_FETCH_SUCCESS = 'USERS_FETCH_SUCCESS'
+export const USERS_FETCH_FAILURE = 'USERS_FETCH_FAILURE'
+
+// Uses the API middlware to get a quote
+export function fetchUsers() {
+  return {
+    [CALL_API]: {
+      endpoint: 'users',
+      types: [USERS_FETCH_REQUEST, USERS_FETCH_SUCCESS, USERS_FETCH_FAILURE]
+    }
+  }
+}
